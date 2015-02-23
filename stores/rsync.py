eb53cccc7e26a01f4b668e0f48edf4c034d7b2ad
Gideon Smeding <gideon.smeding@3ds.com>
2015-02-23 17:18:43 +0100

    Correction to handling of paths in rsync store

--- stores/rsync.py
+++ stores/rsync.py
@@ -1,23 +1,50 @@
-from os.path import exists, join as joinpath, dirname
-import os
+import os.path# import , join as joinpath
+import posixpath
+import tempfile
+
 from subprocess import Popen as popen, PIPE
-from fitlib import fitDir, DataStore
+from fitlib import DataStore
+
+SHOW_PROGRESS_LIMIT = 1e+7 # show progress of transfer for files larger than this
 
 class Store(DataStore):
 
     def __init__(self, *args, **kwds):
-        location = popen('git config fit.datastore.location'.split(), stdout=PIPE).communicate()[0].strip()
-        self.dir = location or joinpath(fitDir, 'store')
+        self.location = popen('git config fit.datastore.location'.split(), stdout=PIPE).communicate()[0].strip()
+        if not self.location :
+            raise Exception("error: No datastore location given, please specify fit.datastore.location in git config.")
+
+        self.dir = tempfile.mkdtemp()
+
+    def __del__(self) :
+        if popen(["rm",  "-r", self.dir]).wait() != 0 :
+            print "Failed to remove temporary folder %s" % self.dir
 
     def get(self, key, dst, size):
-        return popen(['rsync', '--size-only', key, dst]).wait() == 0
+        #print "get(%s, %s)" % (key, dst)
+
+        return popen(['rsync', '--progress', key, dst]).wait() == 0
 
     def put(self, src, dst, size):
-        if exists(src):
-            return popen(['rsync', '-R', src, self.dir]).wait() == 0
+        #print "put(%s, %s)" % (src, dst)
+
+        # copy the file in the temporary directory and execute rsync there
+        # this way, we can create the necessary directories using rsync -R
+        tmpfile = os.path.join(self.dir, dst)
+
+        reporting = "--progress" if size > SHOW_PROGRESS_LIMIT else "--quiet"
+
+        return (
+            popen(['mkdir', '-p', os.path.dirname(tmpfile)]).wait() == 0
+        and popen(['cp', src, tmpfile]).wait() == 0
+        and popen(['rsync', '--progress', '--relative', dst, self.location], cwd=self.dir).wait() == 0
+        )
 
     def check(self, key):
-        path = joinpath(self.dir, key)
+        path = posixpath.join(self.location, key)
 
-        if popen(['rsync', '-q', '--list-only', path], stderr=devnull, stdout=devnull).wait() == 0 :
-            return path
+        # rsync -q isn't quiet when the file doesn't exist
+        # therefore we pipe to /dev/null
+        with file(os.devnull, 'w') as devnull :
+            if popen(['rsync', '--list-only', path], stderr=devnull, stdout=devnull).wait() == 0 :
+                return path
